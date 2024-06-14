b250180434dc584e6f12793be2153a043303f9db
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-14 11:00:32 +0300

    Task 3
    using formed and split

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,16 +1,5 @@
 -- List all bands with Glam rock as their main style, ranked by their longevity
-
-USE holberton;
-
-SELECT 
-    name AS band_name,
-    CASE
-        WHEN split IS NULL THEN 2022 - formed
-        ELSE split - formed
-    END AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    genre = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+	FROM metal_bands
+	WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+	ORDER BY lifespan DESC;
