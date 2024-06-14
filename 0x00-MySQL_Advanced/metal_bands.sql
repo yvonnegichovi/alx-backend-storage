bf0a64fec6c15a8dfcaa012e2d5427daa91390b2
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-14 03:43:41 +0300

    task 2
    import from table dump

---
+++ 0x00-MySQL_Advanced/metal_bands.sql
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>C0PZ3Z8PNYBKX46Z</RequestId><HostId>f7EbPMQQ/gUbWaCWwoWwH8WVXfJYvf0JBNhjl8VhCTrLk+wI/M6xOOpKCQP4ouiDywvDJ0h8tW8=</HostId></Error>
