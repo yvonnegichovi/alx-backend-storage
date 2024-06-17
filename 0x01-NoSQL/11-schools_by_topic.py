198c22ee719e0cc354e04f810293bc3b16ad372b
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-17 21:20:30 +0300

    Task 11
    pymongo - list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This python script uses pymongo to execute MongoDB commands
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Return the list of school having a specific topic
+
+    Args:
+        - mongo_collection: pymongo collection object
+        - topic: will be topic searched
+
+    Return:
+        - list of school having a specific topic
+    """
+    return list(mongo_collection.find({"topic": topic}))
